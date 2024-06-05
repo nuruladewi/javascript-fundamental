8ee603928dc38af1c4e35b6ef91dbee712a14c02
Nurul Aulia Dewi <esdawey@gmail.com>
2024-06-05 11:19:01 +0700

    Uploading files

---
+++ lat_variabel_tipedata.js
@@ -0,0 +1,20 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai "John".
+ *  - lastName: bertipe data string, dengan nilai "Doe".
+ *  - age: bertipe data number, dengan nilai 25.
+ *  - isMarried: bertipe data boolean, dengan nilai true.
+ */
+
+// Tulis kode di bawah ini
+
+let firstName = "John";
+let lastName = "Doe";
+let age = 25;
+let isMarried = true;
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(isMarried);
